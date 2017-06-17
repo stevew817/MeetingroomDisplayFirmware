c39f42c9bbf68a5f61c824bd85e5a2371ee4bddf
Steven Cooreman <steven.cooreman@gmail.com>
2017-06-17 16:37:20 +0200

    Do a proper event-queue-based main loop
    This avoids reading memory from main that might concurrently be updated by another thread

--- main.cpp
+++ main.cpp
@@ -71,35 +71,41 @@ MbedClient mbed_client(device);
 InterruptIn obs_button(SW1);
 
 // Network interaction must be performed outside of interrupt context
-Semaphore updates(0);
 volatile bool registered = false;
 volatile bool clicked = false;
 volatile bool new_data = false;
 osThreadId mainThread;
 
+typedef enum {
+    TIME_UPDATE,
+    ROOM_UPDATE,
+    CUR_OWNER_UPDATE,
+    CUR_TIME_UPDATE,
+    NEXT_OWNER_UPDATE,
+    NEXT_TIME_UPDATE,
+    BUTTON_CLICKED,
+    UNREGISTER
+} ProgramEventType_t;
+
+typedef struct {
+    ProgramEventType_t event;
+    void* data;
+} ProgramEvent_t;
+
+Queue<ProgramEvent_t, 16> evqueue;
+
 void unregister() {
-    registered = false;
-    updates.release();
+    ProgramEvent_t* event = (ProgramEvent_t*) malloc(sizeof(ProgramEvent_t));
+    if (event == NULL) return;
+    event->event = UNREGISTER;
+    evqueue.put(event);
 }
 
 void button_clicked() {
-    clicked = true;
-    updates.release();
-}
-
-void data_updated(void *argument) {
-    if (argument) {
-        M2MResource::M2MExecuteParameter* param = (M2MResource::M2MExecuteParameter*)argument;
-        String object_name = param->get_argument_object_name();
-        uint16_t object_instance_id = param->get_argument_object_instance_id();
-        String resource_name = param->get_argument_resource_name();
-        int payload_length = param->get_argument_value_length();
-        const uint8_t* payload = param->get_argument_value();
-        printf("Resource: %s/%d/%s executed\n", object_name.c_str(), object_instance_id, resource_name.c_str());
-        printf("Payload: %.*s\n", payload_length, payload);
-    }
-    new_data = true;
-    updates.release();
+    ProgramEvent_t* event = (ProgramEvent_t*) malloc(sizeof(ProgramEvent_t));
+    if (event == NULL) return;
+    event->event = BUTTON_CLICKED;
+    evqueue.put(event);
 }
 
 /*
@@ -329,40 +335,45 @@ public:
         meetingroommonitor_inst = meetingroommonitor_obj->create_object_instance();
 
         // 5527 = Text
-        M2MResource* current_owner_res = meetingroommonitor_inst->create_dynamic_resource("32100", "CurrentOwner",
+        current_owner_res = meetingroommonitor_inst->create_dynamic_resource("32100", "CurrentOwner",
             M2MResourceInstance::STRING, true);
         // read and write
         current_owner_res->set_operation(M2MBase::GET_PUT_ALLOWED);
         // set initial owner
         current_owner_res->set_value((const uint8_t*)"Free", 5);
+        current_owner_res->set_value_updated_function(value_updated_callback(this, &MeetingRoomMonitorResource::update_current_owner));
         
-        M2MResource* current_slot_res = meetingroommonitor_inst->create_dynamic_resource("32101", "CurrentSlot",
+        current_slot_res = meetingroommonitor_inst->create_dynamic_resource("32101", "CurrentSlot",
             M2MResourceInstance::STRING, true);
         // read and write
         current_slot_res->set_operation(M2MBase::GET_PUT_ALLOWED);
         // set initial owner
         current_slot_res->set_value((const uint8_t*)"00:00 - 00:00", 14);
+        current_slot_res->set_value_updated_function(value_updated_callback(this, &MeetingRoomMonitorResource::update_current_slot));
         
-        M2MResource* next_owner_res = meetingroommonitor_inst->create_dynamic_resource("32102", "NextOwner",
+        next_owner_res = meetingroommonitor_inst->create_dynamic_resource("32102", "NextOwner",
             M2MResourceInstance::STRING, true);
         // read and write
         next_owner_res->set_operation(M2MBase::GET_PUT_ALLOWED);
         // set initial owner
         next_owner_res->set_value((const uint8_t*)"Free", 5);
+        next_owner_res->set_value_updated_function(value_updated_callback(this, &MeetingRoomMonitorResource::update_next_owner));
         
-        M2MResource* next_slot_res = meetingroommonitor_inst->create_dynamic_resource("32103", "NextSlot",
+        next_slot_res = meetingroommonitor_inst->create_dynamic_resource("32103", "NextSlot",
             M2MResourceInstance::STRING, true);
         // read and write
         next_slot_res->set_operation(M2MBase::GET_PUT_ALLOWED);
         // set initial owner
         next_slot_res->set_value((const uint8_t*)"00:00 - 00:00", 14);
+        next_slot_res->set_value_updated_function(value_updated_callback(this, &MeetingRoomMonitorResource::update_next_slot));
         
-        M2MResource* roomname_res = meetingroommonitor_inst->create_dynamic_resource("32104", "RoomName",
+        roomname_res = meetingroommonitor_inst->create_dynamic_resource("32104", "RoomName",
             M2MResourceInstance::STRING, true);
         // read and write
         roomname_res->set_operation(M2MBase::GET_PUT_ALLOWED);
         // set initial owner
         roomname_res->set_value((const uint8_t*)"Uninit", 7);
+        roomname_res->set_value_updated_function(value_updated_callback(this, &MeetingRoomMonitorResource::update_roomname));
 
         // there's not really an execute LWM2M ID that matches... hmm...
         M2MResource* exec_res = meetingroommonitor_inst->create_dynamic_resource("5850", "Update",
@@ -370,7 +381,7 @@ public:
         // we allow executing a function here...
         exec_res->set_operation(M2MBase::POST_ALLOWED);
         // when a POST comes in, we want to execute the led_execute_callback
-        exec_res->set_execute_function(execute_callback(this, &MeetingRoomMonitorResource::newdata));
+        // exec_res->set_execute_function(execute_callback(this, &MeetingRoomMonitorResource::newdata));
     }
 
     ~MeetingRoomMonitorResource() {
@@ -384,13 +395,110 @@ public:
         return meetingroommonitor_inst;
     }
 
-    void newdata(void* argument) {
-        data_updated(argument);
+    void update_current_owner(const char* argument) {
+        ProgramEvent_t* event = (ProgramEvent_t*) malloc(sizeof(ProgramEvent_t));
+        if (event == NULL) return;
+
+        size_t length = current_owner_res->value_length() + 1;
+        void* dataPtr = malloc(length);
+        if (dataPtr == NULL) {
+            free((void*)event);
+            return;
+        }
+
+        memcpy(dataPtr, current_owner_res->value(), length);
+        ((char*)dataPtr)[length-1] = 0x00;
+
+        event->event = CUR_OWNER_UPDATE;
+        event->data = dataPtr;
+        evqueue.put(event);
+    }
+
+    void update_current_slot(const char* argument) {
+        ProgramEvent_t* event = (ProgramEvent_t*) malloc(sizeof(ProgramEvent_t));
+        if (event == NULL) return;
+
+        size_t length = current_slot_res->value_length() + 1;
+        void* dataPtr = malloc(length);
+        if (dataPtr == NULL) {
+            free((void*)event);
+            return;
+        }
+
+        memcpy(dataPtr, current_slot_res->value(), length);
+        ((char*)dataPtr)[length-1] = 0x00;
+
+        event->event = CUR_TIME_UPDATE;
+        event->data = dataPtr;
+        evqueue.put(event);
+    }
+
+    void update_next_slot(const char* argument) {
+        ProgramEvent_t* event = (ProgramEvent_t*) malloc(sizeof(ProgramEvent_t));
+        if (event == NULL) return;
+
+        size_t length = next_slot_res->value_length() + 1;
+        void* dataPtr = malloc(length);
+        if (dataPtr == NULL) {
+            free((void*)event);
+            return;
+        }
+
+        memcpy(dataPtr, next_slot_res->value(), length);
+        ((char*)dataPtr)[length-1] = 0x00;
+
+        event->event = NEXT_TIME_UPDATE;
+        event->data = dataPtr;
+        evqueue.put(event);
+    }
+
+    void update_next_owner(const char* argument) {
+        ProgramEvent_t* event = (ProgramEvent_t*) malloc(sizeof(ProgramEvent_t));
+        if (event == NULL) return;
+
+        size_t length = next_owner_res->value_length() + 1;
+        void* dataPtr = malloc(length);
+        if (dataPtr == NULL) {
+            free((void*)event);
+            return;
+        }
+
+        memcpy(dataPtr, next_owner_res->value(), length);
+        ((char*)dataPtr)[length-1] = 0x00;
+
+        event->event = NEXT_OWNER_UPDATE;
+        event->data = dataPtr;
+        evqueue.put(event);
+    }
+
+    void update_roomname(const char* argument) {
+        ProgramEvent_t* event = (ProgramEvent_t*) malloc(sizeof(ProgramEvent_t));
+        if (event == NULL) return;
+
+        size_t length = roomname_res->value_length() + 1;
+        void* dataPtr = malloc(length);
+        if (dataPtr == NULL) {
+            free((void*)event);
+            return;
+        }
+
+        memcpy(dataPtr, roomname_res->value(), length);
+        ((char*)dataPtr)[length-1] = 0x00;
+
+        event->event = ROOM_UPDATE;
+        event->data = dataPtr;
+        evqueue.put(event);
     }
 
 private:
     M2MObject* meetingroommonitor_obj;
     M2MObjectInstance* meetingroommonitor_inst;
+
+    M2MResource* current_owner_res;
+    M2MResource* current_slot_res;
+    M2MResource* next_owner_res;
+    M2MResource* next_slot_res;
+    M2MResource* roomname_res;
 };
 
 // Entry point to the program
@@ -447,19 +555,19 @@ Add MBEDTLS_NO_DEFAULT_ENTROPY_SOURCES and MBEDTLS_TEST_NULL_ENTROPY in mbed_app
     
     // Try to get current time. If successful, we have internet connectivity
     NTPClient ntp(network);
-    time_t timestamp = ntp.get_timestamp();
+    time_t ts = ntp.get_timestamp();
     
-    while(timestamp <= 0) {
-        timestamp = ntp.get_timestamp();
+    while(ts <= 0) {
+        ts = ntp.get_timestamp();
         
-        if (timestamp < 0) {
-            printf("An error occurred when getting the time. Code: %ld\r\n", timestamp);
+        if (ts < 0) {
+            printf("An error occurred when getting the time. Code: %ld\r\n", ts);
         }
         printf("Waiting 10 seconds before trying again...\r\n");
         wait(10.0);        
     }
-    printf("Current time is %s\r\n", ctime(&timestamp));
-    set_time(timestamp);
+    printf("Current time is %s\r\n", ctime(&ts));
+    set_time(ts);
 
     // we create our LWM2M resources
     ButtonResource button_resource;
@@ -492,107 +600,158 @@ Add MBEDTLS_NO_DEFAULT_ENTROPY_SOURCES and MBEDTLS_TEST_NULL_ENTROPY in mbed_app
     mbed_client.test_register(register_object, object_list);
     registered = true;
 
-    while (true) {
-        updates.wait(30000);
-        if(registered) {
-            if(!clicked && !new_data) {
-                mbed_client.test_update_register();
-            }
-            if(new_data) {
-	            new_data = false;
-	            display.fillScreen(0);
-	            // values in mbed Client are all buffers, and we need a vector of int's
-	            uint8_t* buffIn = NULL;
-	            uint32_t sizeIn;
-	            
-	            M2MObjectInstance* inst = meetingroommonitor_resource.get_object()->object_instance();
-                if(inst == NULL) {
-                    printf("NullErrorI\n");
-                    continue;
-                }
-	            M2MResource* res = inst->resource("32100");
-	            if(res == NULL) {
-                    printf("NullErrorR\n");
-                    continue;
-                }
-	            display.setFont(&SansSerif_plain_60);
-	            display.setCursor(200,60);
-	            
-	            time_t timestamp= time(NULL);
-	            struct tm *tmp = gmtime(&timestamp);
-	            display.printf("%02d:%02d", tmp->tm_hour, tmp->tm_min);
-	            
-	            display.setFont(&SansSerif_plain_24);
-	            display.setCursor(230,84);
-	            display.printf("%02d/%02d/%02d", tmp->tm_mday, tmp->tm_mon + 1, tmp->tm_year%100);
-	            
-	            display.setFont(&SansSerif_plain_24);
-	            display.setCursor(0,0);
-	            
-	            res = inst->resource("32104");
-                buffIn = res->value();
-                sizeIn = res->value_length();
-                if (sizeIn > 0) {
-                    display.printf("\n%s\n\n", buffIn);
-                }
-	            
-	            res = inst->resource("32103");
-	            buffIn = res->value();
-                sizeIn = res->value_length();
-                if (sizeIn > 0) {
-                    display.printf("Next:\n%s\n", buffIn);
-                }
-	            
-	            res = inst->resource("32102");
-	            buffIn = res->value();
-                sizeIn = res->value_length();
-                if (sizeIn > 0) {
-    	            display.printf("%s", buffIn);
-                }
-	            
-	            display.setFont(&SansSerif_plain_36);
-	            display.setCursor(80,200);
-	            res = inst->resource("32101");
-	            buffIn = res->value();
-                sizeIn = res->value_length();
-                if (sizeIn > 0) {
-    	            display.printf("%s", buffIn);
-                }
-	            
-	            display.setFont(&SansSerif_plain_36);
-	            display.setCursor(10,236);
-	            res = inst->resource("32100");
-	            buffIn = res->value();
-                sizeIn = res->value_length();
-                if (sizeIn > 0) {
-    	            display.printf("%s", buffIn);
-                }
-	            
-	            display.update();
-	            
-	            printf("Updated\n");
-	        } else {
-	            // Update clock
-	            display.fillRect(200,0,200,90,0);
-	            display.setFont(&SansSerif_plain_60);
-	            display.setCursor(200,60);
-	            
-	            time_t timestamp= time(NULL);
-	            struct tm *tmp = gmtime(&timestamp);
-	            display.printf("%02d:%02d", tmp->tm_hour + 2, tmp->tm_min);
-	            display.setFont(&SansSerif_plain_24);
-	            display.setCursor(230,84);
-	            display.printf("%02d/%02d/%02d", tmp->tm_mday, tmp->tm_mon + 1, tmp->tm_year%100);
+    display.fillScreen(0);
+    // values in mbed Client are all buffers, and we need a vector of int's
+    uint8_t* buffIn = NULL;
+    uint32_t sizeIn;
+    
+    M2MObjectInstance* inst = meetingroommonitor_resource.get_object()->object_instance();
+    M2MResource* res = inst->resource("32100");
+    display.setFont(&SansSerif_plain_60);
+    display.setCursor(200,60);
+    
+    time_t timestamp= time(NULL);
+    struct tm *tmp = gmtime(&timestamp);
+    display.printf("%02d:%02d", tmp->tm_hour, tmp->tm_min);
+    
+    display.setFont(&SansSerif_plain_24);
+    display.setCursor(230,84);
+    display.printf("%02d/%02d/%02d", tmp->tm_mday, tmp->tm_mon + 1, tmp->tm_year%100);
+    
+    display.setFont(&SansSerif_plain_24);
+    display.setCursor(0,24);
+    
+    res = inst->resource("32104");
+    buffIn = res->value();
+    sizeIn = res->value_length();
+    if (sizeIn > 0) {
+        display.printf("%s", buffIn);
+    }
+    
+    res = inst->resource("32103");
+    buffIn = res->value();
+    sizeIn = res->value_length();
+    display.setCursor(0,72);
+    if (sizeIn > 0) {
+        display.printf("Next:\n%s", buffIn);
+    }
+    
+    res = inst->resource("32102");
+    display.setCursor(0,120);
+    buffIn = res->value();
+    sizeIn = res->value_length();
+    if (sizeIn > 0) {
+        display.printf("%s", buffIn);
+    }
+    
+    display.setFont(&SansSerif_plain_36);
+    display.setCursor(80,200);
+    res = inst->resource("32101");
+    buffIn = res->value();
+    sizeIn = res->value_length();
+    if (sizeIn > 0) {
+        display.printf("%s", buffIn);
+    }
+    
+    display.setFont(&SansSerif_plain_36);
+    display.setCursor(10,236);
+    res = inst->resource("32100");
+    buffIn = res->value();
+    sizeIn = res->value_length();
+    if (sizeIn > 0) {
+        display.printf("%s", buffIn);
+    }
+    
+    display.update();
 
-	            display.update();
-	        }
-	        if(clicked) {
-	            clicked = false;
-	            button_resource.handle_button_click();
-	        }
-        } else {
-            break;
+    while (registered) {
+        osEvent event = evqueue.get(15000);
+        if(event.status == (osStatus)osEventMessage) {
+            ProgramEvent_t* evPtr = (ProgramEvent_t*)event.value.p;
+            switch(evPtr->event) {
+                case TIME_UPDATE:
+                    // Update clock
+                    display.fillRect(200,0,200,90,0);
+                    display.setFont(&SansSerif_plain_60);
+                    display.setCursor(200,60);
+                    
+                    timestamp= time(NULL);
+                    tmp = gmtime(&timestamp);
+                    display.printf("%02d:%02d", tmp->tm_hour + 2, tmp->tm_min);
+                    display.setFont(&SansSerif_plain_24);
+                    display.setCursor(230,84);
+                    display.printf("%02d/%02d/%02d", tmp->tm_mday, tmp->tm_mon + 1, tmp->tm_year%100);
+
+                    display.update();
+                    break;
+                case ROOM_UPDATE:
+                    display.setFont(&SansSerif_plain_24);
+                    display.setCursor(0,30);
+                    display.fillRect(0,30-24,200,24,0);
+                    display.printf("%s", evPtr->data);
+                    free(evPtr->data);
+                    display.update();
+                    printf("Updated room name\n");
+                    break;
+                case CUR_OWNER_UPDATE:
+                    display.setFont(&SansSerif_plain_36);
+                    display.setCursor(10,236);
+                    display.fillRect(10,236-36,390,36,0);
+                    display.printf("%s", evPtr->data);
+                    free(evPtr->data);
+                    display.update();
+                    printf("Updated current owner\n");
+                    break;
+                case CUR_TIME_UPDATE:
+                    display.setFont(&SansSerif_plain_36);
+                    display.setCursor(80,200);
+                    display.fillRect(80,200-36,320,36,0);
+                    display.printf("%s", evPtr->data);
+                    free(evPtr->data);
+                    display.update();
+                    printf("Updated current slot\n");
+                    break;
+                case NEXT_OWNER_UPDATE:
+                    display.setFont(&SansSerif_plain_24);
+                    display.setCursor(0,120);
+                    display.fillRect(0,120-24,400,24,0);
+                    display.printf("%s", evPtr->data);
+                    free(evPtr->data);
+                    display.update();
+                    printf("Updated next owner\n");
+                    break;
+                case NEXT_TIME_UPDATE:
+                    display.setFont(&SansSerif_plain_24);
+                    display.setCursor(0,96);
+                    display.fillRect(0,96-24,400,24,0);
+                    display.printf("%s", evPtr->data);
+                    free(evPtr->data);
+                    display.update();
+                    printf("Updated next slot\n");
+                    break;
+                case BUTTON_CLICKED:
+                    button_resource.handle_button_click();
+                    break;
+                case UNREGISTER:
+                    registered = false;
+                    break;
+                default: break;
+            }
+            free(evPtr);
         }
+        // Update clock
+        display.fillRect(200,0,200,90,0);
+        display.setFont(&SansSerif_plain_60);
+        display.setCursor(200,60);
+        
+        timestamp= time(NULL);
+        tmp = gmtime(&timestamp);
+        display.printf("%02d:%02d", tmp->tm_hour + 2, tmp->tm_min);
+        display.setFont(&SansSerif_plain_24);
+        display.setCursor(230,84);
+        display.printf("%02d/%02d/%02d", tmp->tm_mday, tmp->tm_mon + 1, tmp->tm_year%100);
+
+        display.update();
     }
 
     mbed_client.test_unregister();
